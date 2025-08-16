6b146dc7644be733d93c5bdb7371d075f9423cac
Martin Medrano <mmedranom@tecnocomfenalco.edu.co>
2025-08-15 22:46:20 -0500

    Contenerización del backend mediante docker y empezando a modularizar los endpoints

--- backend/index.js
+++ backend/index.js
@@ -3,40 +3,29 @@ const cors = require('cors')
 require('dotenv').config();
 const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient();
+import usuario from './routes/usuario'
+import categoria from './routes/categoria'
+import movimiento from './routes/movimiento'
 
 const app = express();
-const port  = process.env.PORT || 3001;
+const port  = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/usuario', usuario)
+app.use('/categoria', categoria)
+app.use('/movimiento', movimiento)
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
 
 app.get('/', (req, res) => {
     res.send('we did it!');
 });
 
-app.post('/register', async (req,res)=>{
-    try {
-    const {nombre, email, password} = req.body;
 
-    const usuario = await prisma.usuario.create({
-        data : {nombre,email,password}
-    });
-
-    res.status(200).json(usuario)
-
-    } catch (error) {
-        res.status(500).json({error: error.message})
-    }
-})
-
-app.get('/usuarios', async (req ,res)=>{
-    try {
-        const Usuario = await prisma.usuario.findMany();
-        res.json(Usuario);
-    } catch (error) {
-        res.json({error:error.message});
-    }
-});
 
 //consultar todos los ingresos
 app.get('/movimientos/ingreso/:id', async (req, res)=>{
--- backend/routes/usuario.js
+++ backend/routes/usuario.js
@@ -6,17 +6,26 @@ const app = express();
 
 app.use(express.json())
 
-app.post('/register', async (req, res) =>{
-  try {
-   const {nombre,email,password} = req.body;
+app.post('/register', async (req,res)=>{
+    try {
+    const {nombre, email, password} = req.body;
 
-  const usuario = await prisma.usuario.create({
-    data : {nombre,email,password}
-  });
+    const usuario = await prisma.usuario.create({
+        data : {nombre,email,password}
+    });
 
-  res.json(usuario) 
-  } catch (error) {
-    res.status(500).json({error: error.message})
-  }
+    res.status(200).json(usuario)
+
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
 })
 
+app.get('/usuarios', async (req ,res)=>{
+    try {
+        const Usuario = await prisma.usuario.findMany();
+        res.json(Usuario);
+    } catch (error) {
+        res.json({error:error.message});
+    }
+});
